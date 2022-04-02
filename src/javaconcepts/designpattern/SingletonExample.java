6cc2e132438b3cc6fa2062c028bf3e9e90176bfb
patelraj1278 <patelraj.ps@gmail.com>
2022-04-02 12:56:29 -0400

    Updated code

--- src/javaconcepts/designpattern/SingletonExample.java
+++ src/javaconcepts/designpattern/SingletonExample.java
@@ -1,4 +1,9 @@
 package javaconcepts.designpattern;
 
+/*
+Singleton is a class which has only one instance in whole application and provides a getInstance() method to access the singleton instance.
+There are many classes in JDK which is implemented using Singleton pattern like java.lang.Runtime which provides getRuntime() method to get access of it and used to get free memory and total memory in Java.
+
+ */
 public class SingletonExample {
 }
