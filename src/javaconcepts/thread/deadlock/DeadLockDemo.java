cbe2419ccd39086274a212b2634e5f3fdeac80ef
patelraj1278 <patelraj.ps@gmail.com>
2022-04-07 13:29:47 -0400

    Updated code

--- src/javaconcepts/thread/deadlock/DeadLockDemo.java
+++ src/javaconcepts/thread/deadlock/DeadLockDemo.java
@@ -22,7 +22,7 @@ public class DeadLockDemo {
     }
 }
 
-public class DeadLockFixed {
+class DeadLockFixed {
     /** * Both method are now requesting lock in same order, * first Integer and then String. * You could have also done reverse e.g. first String and then Integer, * both will solve the problem, as long as both method are requesting lock * in consistent order. */
     public void method1() {
         synchronized (Integer.class) {
