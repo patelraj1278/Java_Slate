da28ec7d3fd33a9ac181d174fafa25c0744ebb1e
patelraj1278 <patelraj.ps@gmail.com>
2022-04-08 00:18:04 -0400

    Updated code

---
+++ src/javaconcepts/innerclass/LocalInnerClass.java
@@ -0,0 +1,17 @@
+package javaconcepts.innerclass;
+
+public class LocalInnerClass {
+    private int data=30;//instance variable
+    void display(){
+        class Local{
+            void msg(){System.out.println(data);}
+        }
+        Local l=new Local();
+        l.msg();
+    }
+    public static void main(String args[]){
+        LocalInnerClass obj=new LocalInnerClass();
+        obj.display();
+    }
+}
+
