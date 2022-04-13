f13f26f1bef15e2ce28b6f3d90c56bd0e85b1c0d
patelraj1278 <patelraj.ps@gmail.com>
2022-04-12 21:42:42 -0400

    Updated code

--- src/quicktests/QuickPractice2.java
+++ src/quicktests/QuickPractice2.java
@@ -3,20 +3,45 @@ package quicktests;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class QuickPractice2 {
+
+
+    public boolean anagramOrNot(){
+        String s1 = "hello";
+        String s2 = "eholl";
+
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        Stream.of(c1,c2).forEach(System.out::println);
+
+        BiPredicate<String,String> biPred = (x,y) -> {
+            if(x.equalsIgnoreCase(y)){
+                return true;
+            }else{
+                return false;
+            }
+        };
+
+        return doMatching(c1,c2,biPred);
+
+    }
+
+    public static boolean doMatching(char[] c1, char[] c2 , BiPredicate<String,String> biPred){
+            return biPred.test(String.valueOf(c1),String.valueOf(c2));
+    }
+
     public static void main(String [] args){
-        Integer[] intArray = {1,2,3,4,5};
-        List<Integer> listArray = new ArrayList(Arrays.stream(intArray).toList());
 
-        List<Integer> listArray1 =  Arrays.stream(intArray).mapToInt(x -> x.intValue()).boxed().collect(Collectors.toList());
-
-        listArray1.forEach(System.out::println);
-
-        listArray1.stream().mapToInt(x -> x.shortValue()).boxed().collect(Collectors.toList());
-
-
+        QuickPractice2 qp = new QuickPractice2();
+        System.out.println(qp.anagramOrNot());
     }
 }
