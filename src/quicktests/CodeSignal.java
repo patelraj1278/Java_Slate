000720ef489b9f780775857fbf91faf57fd6cfe1
patelraj1278 <patelraj.ps@gmail.com>
2022-04-18 18:35:50 -0400

    Update code

--- src/quicktests/CodeSignal.java
+++ src/quicktests/CodeSignal.java
@@ -1,12 +1,7 @@
 package quicktests;
 
-import java.awt.*;
 import java.util.*;
-import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CodeSignal {
 
