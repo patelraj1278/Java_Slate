ccaf16979a6fc537902801e35508a4ce075fc8cc
Raj Patel <raj12789@gmail.com>
2022-04-24 16:13:42 -0400

    Update code

--- src/softwaredependency/SoftwareDependency.java
+++ src/softwaredependency/SoftwareDependency.java
@@ -2,6 +2,19 @@ package softwaredependency;
 
 public class SoftwareDependency {
 
+    enum COMMAND {
+        DEPEND,
+        INSTALL,
+        REMOVE,
+        LIST,
+        END;
 
+        COMMAND(){
 
+        }
+    }
+
+    public static void main(String [] args){
+
+    }
 }
