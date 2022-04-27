a3bd585f015406fd5c1fd29acc0791cd3e9d3955
patelraj1278 <patelraj.ps@gmail.com>
2022-04-26 23:51:49 -0400

    update code

---
+++ src/quicktests/CodeSignalTest1.java
@@ -0,0 +1,38 @@
+package quicktests;
+
+public class CodeSignalTest1 {
+    String solution(String s) {
+        String result = s;
+        if(result.length() == 0 ){
+            return result;
+        }
+
+        for(int i=1;i <= s.length();i++){
+            String str = s.substring(0,i);
+            if(isPalin(str)){
+                if(str.length() > 2){
+                    result=str.substring(i);
+                    solution(s.substring(i));
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean isPalin(String s){
+        StringBuffer sb = new StringBuffer();
+        sb.append(s);
+        if(s.length() > 0 && sb.reverse().toString().equals(s)){
+            sb.delete(0,s.length());
+            return true;
+        }else{
+            sb.delete(0,s.length());
+            return false;
+        }
+    }
+
+    public static void main(String [] args){
+        CodeSignalTest1 cs = new CodeSignalTest1();
+        System.out.println("Result=>"+cs.solution(""));
+    }
+}
