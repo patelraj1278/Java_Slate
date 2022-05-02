01492e99953aa6cdd09d1d44cafbd11e236bbb9f
patelraj1278 <patelraj.ps@gmail.com>
2022-05-01 23:59:40 -0400

    Update code

--- src/quicktests/CodeSignalTest3.java
+++ src/quicktests/CodeSignalTest3.java
@@ -1,11 +1,12 @@
 package quicktests;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 public class CodeSignalTest3 {
-    int solution(int[] a, int[] b) {
+    static int solution(int[] a, int[] b) {
 
         Set<Integer> findMax = new HashSet<>();
         int diff = 0;
@@ -16,11 +17,7 @@ public class CodeSignalTest3 {
                     int[] c = Arrays.copyOf(a, a.length);
                     c[i]=a[j];
                     for(int k=0; k < c.length; k++){
-                        if(c[k] < b[k]){
-                            diff += b[k] - c[k];
-                        }else{
-                            diff += c[k] - b[k];
-                        }
+                            diff += Math.abs(b[k] - c[k]);
                     }
                     findMax.add(diff);
                 }
@@ -33,7 +30,44 @@ public class CodeSignalTest3 {
 
     public static void main(String args[]){
         CodeSignalTest3 cs = new CodeSignalTest3();
-        System.out.println(cs.solution(new int[]{1,3,5}, new int[]{5,3,1}));
+        long before = System.currentTimeMillis();
+        System.out.println(cs.solution(new int[]{1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76}, new int[]{5,1,3,34,6,7,24,6,7,9,23,5,76,8,2,4,65,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76}));
+        long after = System.currentTimeMillis();
+        System.out.println(after-before);
     }
 
+}
+
+class ArrayDifference {
+    public static void main(String args[]){
+        int[] a= new int[] {1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76};
+        int[] b= new int[] {5,1,3,34,6,7,24,6,7,9,23,5,76,8,2,4,65,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76,1,3,5,6,2,4,6,2,5,64,74,14,6,8,12,54,76};
+
+//        System.out.println("Before Current date time in milliseconds : "+new Date().getTime());
+        long start1 = System.currentTimeMillis();
+        System.out.println(getTheMinimumDifference(a,b));
+        long end1 = System.currentTimeMillis();
+        System.out.println("Elapsed Time in mulli" +
+                " seconds: "+ (end1-start1));
+    }
+
+    public static int getTheMinimumDifference(int[] a, int[] b){
+        int result=Integer.MAX_VALUE;
+        int mainTotal=0;
+        if(a.length != b.length)
+            return 0;
+        int temp[] = new int[a.length];
+        for(int i=0;i<b.length;i++){
+            temp[i] = Math.abs(a[i] - b[i]);
+            mainTotal += temp[i];
+        }
+        for(int i=0;i<a.length;i++){
+            int total = Integer.MAX_VALUE;
+            for(int j=0;j<a.length;j++) {
+                total = Math.min(total,(mainTotal - temp[j])  + Math.abs(a[i] - a[j]));
+            }
+            result = Math.min(total,result);
+        }
+        return result;
+    }
 }
