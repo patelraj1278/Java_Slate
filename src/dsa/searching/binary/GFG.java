6f49636a61c6fbea20c55ee0a03b207d6734eba3
patelraj1278 <patelraj.ps@gmail.com>
2023-03-15 12:32:28 -0400

    DSA Practice

--- src/dsa/searching/binary/GFG.java
+++ src/dsa/searching/binary/GFG.java
@@ -1,2 +1,57 @@
-package dsa.searching.binary;public class GFG {
+package dsa.searching.binary;
+
+import java.util.Arrays;
+
+public class GFG {
+
+    static int binaryRecurrsiveSearch(int arr[], int low, int high, int key) {
+            if(high >= 1){
+                 int mid = low + (high - 1) / 2;
+
+                 if(arr[mid] == key){
+                        System.out.println("Key Fonund");
+                        return mid;
+                 }
+
+                 if(arr[mid] > key) {
+                     return binaryRecurrsiveSearch(arr,low, mid - 1 , key);
+                 }
+                 return binaryRecurrsiveSearch(arr,mid + 1, high , key);
+            }
+        return -1;
+    }
+
+    static int binaryLinerSearch(int[] arr , int key){
+             int low = 0;
+             int high= arr.length -1 ;
+            while(low <= high){
+                int mid = low + (high - 1) / 2;
+                if(arr[mid] == key){
+                    return mid;
+                }
+                if(arr[mid] < key){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+           return -1;
+    }
+    public static void main(String [] args){
+        int v[]={3, 1, 4, 5, 6};
+        /*int result = binaryRecurrsiveSearch(Arrays.stream(v).sorted().toArray(),0, v.length - 1, 3);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index "
+                    + result);*/
+
+        int result1 = binaryLinerSearch(Arrays.stream(v).sorted().toArray(),3);
+        if (result1 == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index "
+                    + result1);
+
+    }
 }
