0d4dee0df114763bbdf2df8a4694033b7ba4b5c4
patelraj1278 <patelraj.ps@gmail.com>
2023-05-13 16:34:53 -0400

    Update Code

--- src/dsa/sorting/QuickSortExample.java
+++ src/dsa/sorting/QuickSortExample.java
@@ -1,10 +1,25 @@
 package dsa.sorting;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class QuickSortExample {
 
     static int partition(int[] arr, int low, int high){
         int pivot = arr[high];
-        return -1;
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
     }
     static void quickSort(int[] arr, int low, int high){
             if(low < high){
@@ -16,6 +31,19 @@ public class QuickSortExample {
     }
     public static void main(String [] args){
         int[] arr = { 10, 7, 8, 9, 1, 5 };
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
         quickSort(arr,0, arr.length-1);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+
+        String[] arr1 = {"banana", "apple", "orange", "grape"};
+        System.out.println("Unsorted array: " + Arrays.toString(arr1));
+        Arrays.sort(arr1, new StringLengthComparator());
+        System.out.println("Sorted array: " + Arrays.toString(arr1));
+    }
+}
+
+class StringLengthComparator implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        return Integer.compare(s1.length(), s2.length());
     }
 }
