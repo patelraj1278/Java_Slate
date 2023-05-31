39b098db02bfd81038f166783089770d096947a9
patelraj1278 <patelraj.ps@gmail.com>
2023-05-31 14:06:28 -0400

    Update Code

---
+++ src/dsa/leet/BinarySearch.java
@@ -0,0 +1,22 @@
+package dsa.leet;
+
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String [] args){
+        search(new int[] {1,4,5,6,2},4);
+    }
+}
