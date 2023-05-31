c4a3e78f3e12a4602a2b8279158edb97b3c71a0c
patelraj1278 <patelraj.ps@gmail.com>
2023-05-31 14:02:53 -0400

    Update Code

---
+++ src/dsa/leet/DigitSum.java
@@ -0,0 +1,24 @@
+package dsa.leet;
+
+public class DigitSum {
+    public static int alternateDigitSum(int n) {
+        int firstHalf = 0;
+        int secondHalf = 0;
+        int count = 0;
+        while (n > 0) {
+            secondHalf += n % 10;
+            n /= 10;
+            count++;
+            if (n > 0) {
+                firstHalf += n % 10;
+                n /= 10;
+                count++;
+            }
+        }
+        return count % 2 == 0 ? (firstHalf + -1 * secondHalf) : (secondHalf + -1 * firstHalf);
+    }
+
+    public static void main(String [] args){
+        alternateDigitSum(5);
+    }
+}
