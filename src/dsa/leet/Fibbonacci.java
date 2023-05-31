252e74ea1db7b3af203bade20ec3883d96144959
patelraj1278 <patelraj.ps@gmail.com>
2023-05-31 14:59:46 -0400

    Update Code

---
+++ src/dsa/leet/Fibbonacci.java
@@ -0,0 +1,19 @@
+package dsa.leet;
+
+public class Fibbonacci {
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 0;
+        result[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n];
+    }
+    public static void main(String [] args){
+        fib(12);
+    }
+}
