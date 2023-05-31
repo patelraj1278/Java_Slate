f42eb61ab025d1fabc136771c50366bee03b8793
patelraj1278 <patelraj.ps@gmail.com>
2023-05-31 14:17:46 -0400

    Update Code

---
+++ src/dsa/leet/CountElements.java
@@ -0,0 +1,20 @@
+package dsa.leet;
+
+public class CountElements {
+    public static int countElements(int[] arr) {
+        int[] counter = new int[1001];
+        for (int num : arr) {
+            counter[num]++;
+        }
+        int count = 0;
+        for (int i = 0; i < 1001; i++) {
+            if (i != 1000 && counter[i + 1] != 0) {
+                count += counter[i];
+            }
+        }
+        return count;
+    }
+    public static void main(String [] args){
+        countElements(new int[]{2,1,3,4});
+    }
+}
